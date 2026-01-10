52af450586ed6c85332b687dd2f558fdcc5dfea2
xingling66666 <xingling66666@gmail.com>
2026-01-10 17:52:32 +0800

    Add files via upload

---
+++ fetch-mini.js
@@ -0,0 +1,16 @@
+function request(url,method,responseType,func) {
+
+	if([url,method,responseType,func].indexOf(null) != -1 || /text|json|blob|arrayBuffer|formData/.test(responseType) === false || /GET|POST|PUT|DELETE|HEAD|OPTIONS|PATCH|CONNECT|TRACE/.test(method) === false 
+
+	)
+	{
+    console.error("参数有误")
+    return
+	}
+fetch(url,{
+		method: method
+}).then(res => res[responseType]()).then(data => {
+		func(data)
+	}
+	)
+}<
